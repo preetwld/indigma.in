fae493ecaeac15030ea638b05f5709bfda269885
Preet <ipreeettttt@gmail.com>
2025-01-13 18:31:32 +0530

    4

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import { Button } from '@/components/ui/button'
-import { InstagramIcon, TwitterIcon } from 'lucide-react'
+
 
 
 export default function LandingPage() {
